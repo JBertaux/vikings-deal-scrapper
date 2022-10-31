005c40d085b030b7e22c9ecdc9dc9e1a2bd2f13b
Jérôme Bertaux <JBertaux@users.noreply.github.com>
2022-10-31 10:48:05 +0100

    Add TamperMonkey script

---
+++ deals.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Mobile Vikings Deals
+// @namespace    https://jeromebertaux.be
+// @version      0.1
+// @description  Diplay deals in partner website
+// @author       Jérôme Bertaux
+// @match        http*://*/*
+// @icon         https://mobilevikings.be/static/img/icons/favicons/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/JBertaux/vikings-deal-scrapper/main/deals.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function addBanner(json) {
+        if (shouldBannerBeAdded(json)) {
+            var banner = document.createElement("div");
+            banner.innerHTML = "There is a Viking Deals";
+            banner.style.color = 'black';
+            banner.style.backgroundColor = 'red';
+            banner.style.textAlign = 'center';
+
+            document.body.insertBefore(banner, document.body.childNodes[0]);
+        }
+    }
+
+    function shouldBannerBeAdded(json) {
+        let currentUrl = window.location.href;
+        for (let deal of json) {
+            if (deal['enabled']) {
+                for (let url of deal['urls']) {
+                    if (currentUrl.includes(url)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    fetch('https://raw.githubusercontent.com/JBertaux/vikings-deal-scrapper/main/data/data.json')
+        .then((response) => response.json())
+        .then((json) => addBanner(json));
+
+})();
